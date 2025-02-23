ff375bffbd9e3e6b608c803eb3ee50ba3d0a1090
Sneha Rajan <97793877+sneharajan7@users.noreply.github.com>
2025-02-23 11:24:06 +0530

    Delete .vscode/halfpy.py

--- .vscode/halfpy.py
+++
@@ -1,9 +0,0 @@
-def print_pyramid(size):
-    for row in range(0, size):
-        for col in range(0, row+1):
-            print("*", end=" ")
-        print("")
-
-
-size = int(input("Enter the size of the Pyramid : "))
-print_pyramid(size)
